6df9475ff475303b1a58eef51ef0d328e8e8801e
Niariasve <nestorariasve@gmail.com>
2023-11-07 18:09:43 -0500

    División de paquetes en Modelo Vista Controlador

--- AppContactos/src/main/java/module-info.java
+++ AppContactos/src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module ec.edu.espol.appcontactos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens ec.edu.espol.appcontactos to javafx.fxml;
     exports ec.edu.espol.appcontactos;
+    
+    opens ec.edu.espol.controlador to javafx.fxml;
+    exports ec.edu.espol.controlador;
 }
