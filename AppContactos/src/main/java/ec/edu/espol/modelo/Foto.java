6a2a0cd31527d7ea70473e48f0ab93babdd70b64
jair-chaguay <jchaguay@espol.edu.ec>
2023-11-07 19:20:17 -0500

    Update Foto.java

--- AppContactos/src/main/java/ec/edu/espol/modelo/Foto.java
+++ AppContactos/src/main/java/ec/edu/espol/modelo/Foto.java
@@ -9,5 +9,17 @@ package ec.edu.espol.modelo;
  * @author alexc
  */
 public class Foto {
+    private String Url;
     
+    public Foto(String Url){
+        this.Url=Url;
+    }
+    
+    public String getUrl(){
+        return Url;
+    }
+    
+    public void setUrl(String Url){
+        this.Url=Url;
+    }
 }
