25245afd116e0b18e596510be4d8fcc5f55c5772
angmcruz <angmcruz@espol.edu.ec>
2023-11-14 00:39:25 -0500

    arreglando modoListaVista

--- AppContactos/src/main/java/ec/edu/espol/controlador/ContactosListaController.java
+++ AppContactos/src/main/java/ec/edu/espol/controlador/ContactosListaController.java
@@ -5,14 +5,24 @@
 package ec.edu.espol.controlador;
 
 import ec.edu.espol.appcontactos.App;
+import ec.edu.espol.modelo.Contacto;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Box;
+import javafx.scene.text.Font;
 
 /**
  * FXML Controller class
@@ -25,20 +35,69 @@ public class ContactosListaController implements Initializable {
     private ScrollPane listaFP;
     @FXML
     private Label numContactosReg;
+    @FXML
+    private ImageView agregar;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        List<Contacto> contactos = ContactosController.contactos;
+        VBox medio = new VBox();
+        int num = contactos.size();
+        numContactosReg.setText(String.valueOf(num));
+
+        for (int i = 0; i < num; i++) {
+            BorderPane contenedor = new BorderPane();
+            Label textoContacto = new Label();
+
+            String nombreCompleto;
+
+            if (contactos.get(i).getEmpresa() != null && !contactos.get(i).getEmpresa().isEmpty()) {
+
+                nombreCompleto = contactos.get(i).getEmpresa();
+
+            } else {
+
+                nombreCompleto = contactos.get(i).getNombre() + " " + contactos.get(i).getApellido();
+
+            }
+
+            textoContacto = new Label(nombreCompleto);
+
+            BorderPane.setMargin(textoContacto, new javafx.geometry.Insets(20, 0, 20, 0)); // para Margen
+            contenedor.setCenter(textoContacto);
+            contenedor.setStyle("-fx-border-color: black; -fx-border-width: 1px;");
+            contenedor.setAlignment(textoContacto, Pos.CENTER_LEFT);
+
+            medio.getChildren().add(contenedor);
+
+   
+            HBox.setHgrow(contenedor, Priority.ALWAYS);
+
+        }
+
+        listaFP.setContent(medio);
+
+    }
 
     @FXML
-    private void cambiarVistaContactos(MouseEvent event) {
+    private void cambiarVistaContactos(MouseEvent event
+    ) {
         try {
             App.setRoot("contactos");
-        } catch (IOException ex) {}
+        } catch (IOException ex) {
+        }
     }
-    
+
+    @FXML
+    private void entrarAgregar(MouseEvent ev
+    ) {
+        try {
+            App.setRoot("agregarContacto");
+        } catch (IOException ex) {
+        }
+    }
+
 }
