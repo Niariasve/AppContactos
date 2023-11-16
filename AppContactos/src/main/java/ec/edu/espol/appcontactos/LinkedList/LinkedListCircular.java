7accb27138600207461896e5a7dd053b6c1ee7fc
Niariasve <nestorariasve@gmail.com>
2023-11-15 20:22:04 -0500

    Update LinkedListCircular.java

--- AppContactos/src/main/java/ec/edu/espol/appcontactos/LinkedList/LinkedListCircular.java
+++ AppContactos/src/main/java/ec/edu/espol/appcontactos/LinkedList/LinkedListCircular.java
@@ -14,7 +14,7 @@ import java.util.NoSuchElementException;
  *
  * @author alexc
  */
-public class LinkedListContacto<E> implements List<E> {
+public class LinkedListCircular<E> implements List<E> {
 
     private Nodo<E> first;
     private Nodo<E> last;
