b774b84a726e0414724a14d8c0f83eba5252bd90
jair-chaguay <jchaguay@espol.edu.ec>
2023-11-29 19:47:43 -0500

    Metodos filtrar

--- AppContactos/src/main/java/ec/edu/espol/controlador/ContactosListaController.java
+++ AppContactos/src/main/java/ec/edu/espol/controlador/ContactosListaController.java
@@ -61,7 +61,7 @@ public class ContactosListaController implements Initializable {
     private TextField Busqueda;
 
     @FXML
-    private Button filtrarA;
+    private Button filtrarB;
 
     /**
      * Initializes the controller class.
@@ -91,11 +91,8 @@ public class ContactosListaController implements Initializable {
 
         ObservableList<String> opcionesLista = FXCollections.observableArrayList(
                 "Apellido y Primer Nombre",
-                "Cantidad de Atributos",
-                "Fecha de Cumpleaños",
                 "Empresa",
-                "País de residencia",
-                "TipoContacto"
+                "Dirección"
         );
 
         Opciones.setItems(opcionesLista);
@@ -122,61 +119,78 @@ public class ContactosListaController implements Initializable {
 
     @FXML
     private void filtrar(MouseEvent event) {
-        if (Busqueda.getText() != "") {
-            filtrarConTextBox();
+        if (Busqueda.getText().isEmpty()) {
+            filtrarSinTextBox();
         } else {
-            
+            filtrarConTextBox();
         }
     }
 
-    private boolean cumpleConFiltro(Contacto contacto, String filtro) {
-        return contacto.getNombre().toLowerCase().contains(filtro) || contacto.getApellido().toLowerCase().contains(filtro);
+    private boolean cumpleConFiltro(Contacto contacto, String filtro, String OpcionSelecionada) {
+        switch (OpcionSelecionada) {
+
+            case "Apellido y Primer Nombre":
+                System.out.println("Filtrando por Apellido y Primer Nombre");
+                System.out.println("Nombre del contacto: " + contacto.getNombre());
+                System.out.println("Apellido del contacto: " + contacto.getApellido());
+                return contacto.getNombre().toLowerCase().contains(filtro) || contacto.getApellido().toLowerCase().contains(filtro);
+            case "Empresa":
+                return contacto.getEmpresa().toLowerCase().contains(filtro);
+            case "Dirección":
+                return contacto.getDireccion().toLowerCase().contains(filtro);
+            // Puedes agregar más casos según tus necesidades
+            default:
+                return false;
+        }
     }
 
     private void filtrarConTextBox() {
-        String filtro = Busqueda.getText().toLowerCase(); // Obtener el texto del TextField y convertirlo a minúsculas
+        listaFP.setContent(null);
+        VBox medio = new VBox();
 
-        if (filtro.isEmpty()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText("Error, no ha ingresado ningún valor a filtrar");
-            Optional<ButtonType> result = alert.showAndWait();
-        } else {
-            LinkedListCircular<Contacto> listaFiltrada = new LinkedListCircular<>();
-            for (Contacto contacto : contactos) {
-                if (cumpleConFiltro(contacto, filtro)) {
-                    listaFiltrada.add(contacto);
-                }
-            }
-            for (Contacto contacto : listaFiltrada) {
-                System.out.println(contacto);
+        String filtro = Busqueda.getText().toLowerCase();
+        String opcionSeleccionada = Opciones.getValue();
+
+        LinkedListCircular<Contacto> listaFiltrada = new LinkedListCircular<>();
+        for (Contacto contacto : contactos) {
+            if (cumpleConFiltro(contacto, filtro, opcionSeleccionada)) {
+                listaFiltrada.add(contacto);
             }
         }
+
+        for (int i = 0; i < listaFiltrada.size(); i++) {
+            crearContenedorDeContacto(listaFiltrada, i, medio);
+        }
+
+        listaFP.setContent(medio);
     }
-    
+
     private void filtrarSinTextBox() {
-        
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText("Error, no ha ingresado ningún valor a filtrar");
+        Optional<ButtonType> result = alert.showAndWait();
     }
-    
+
     private void crearContenedorDeContacto(List<Contacto> contactos, int i, VBox medio) {
-            VBox contenedor = new VBox();
-            contenedor.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-width: 1px;"); // ESTILO DEL CONTENEDOR
-            contenedor.setPrefSize(395, 40); // TAMANO DEL CONTENEDOR 
-            contenedor.setAlignment(Pos.CENTER_LEFT); //ALINEACION IZQ CENTRO
+        VBox contenedor = new VBox();
+        contenedor.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-width: 1px;"); // ESTILO DEL CONTENEDOR
+        contenedor.setPrefSize(395, 40); // TAMANO DEL CONTENEDOR 
+        contenedor.setAlignment(Pos.CENTER_LEFT); //ALINEACION IZQ CENTRO
 
-            String nombreCompleto;
-            if (contactos.get(i).getEmpresa() != null && !contactos.get(i).getEmpresa().isEmpty()) {
-                nombreCompleto = contactos.get(i).getEmpresa();
+        String nombreCompleto;
+        if (contactos.get(i).getEmpresa() != null && !contactos.get(i).getEmpresa().isEmpty()) {
+            nombreCompleto = contactos.get(i).getEmpresa();
 
-            } else {
-                nombreCompleto = contactos.get(i).getNombre() + " " + contactos.get(i).getApellido();
+        } else {
+            nombreCompleto = contactos.get(i).getNombre() + " " + contactos.get(i).getApellido();
 
-            }
+        }
 
-            Label textoContacto = new Label(nombreCompleto);
-            textoContacto.setFont(Font.font("Segoe UI", 14));  // FUENTE DE LETRA 
-            contenedor.getChildren().add(textoContacto);
-            medio.getChildren().add(contenedor);
+        Label textoContacto = new Label(nombreCompleto);
+        textoContacto.setFont(Font.font("Segoe UI", 14));  // FUENTE DE LETRA 
+        contenedor.getChildren().add(textoContacto);
+        medio.getChildren().add(contenedor);
     }
 }
